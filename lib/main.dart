c28aaf820ee3e101637fcf000ad3f1a6250130cb
Zachary Gonzalez <zacharygonzalez1234@gmail.com>
2022-08-06 15:05:52 -0700

    Enivromental Variables Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
@@ -9,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Open Transit',
+     
       theme: ThemeData(),
       debugShowCheckedModeBanner: true,
     );
